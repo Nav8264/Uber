74331ef7cf8e717a63f39ec2c6d04f30fd20b888
Navneet <navneet.singh0863@gmail.com>
2025-02-18 21:46:26 +0530

    initial Commit

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+const connectToDb=require('./db/db')
+
+connectToDb()
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+module.exports = app;
